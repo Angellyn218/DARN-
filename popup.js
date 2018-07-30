1f73cad8a3a01509e29e57412eb566b31a28f0c4
diyapathak <diya.pathak.4@gmail.com>
2018-07-30 10:19:20 -0700

    Add files via upload

--- popup.js
+++ popup.js
@@ -6,12 +6,52 @@
 //     });
 // });
 /* Get all elements with class="close" */
-var closebtns = document.getElementsByClassName("close");
-var i;
+// var closebtns = document.getElementsByClassName("close");
+// var i;
 
 /* Loop through the elements, and hide the parent, when clicked on */
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
-  this.parentElement.style.display = 'none';
-});
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+// Create a new list item when clicking on the "Add" button
+var element = document.getElementById("button");
+element.addEventListener("click", function(){ newElement() });
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("userInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("userInput").value = "";
+
+  var span = document.createElement("span");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
